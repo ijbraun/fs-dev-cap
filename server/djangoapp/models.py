8c963c38898f95eeebf82fd041f6aa8fa3a8c626
ijbraun <9153212+ijbraun@users.noreply.github.com>
2025-05-07 14:15:32 -0400

    added models, views and data population

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,8 +12,28 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.name
 
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    TYPES = [
+        ('SEDAN','Sedan'),
+        ('SUV','Suv'),
+        ('WAGON','Wagon'),
+        ('COUPE','Coupe'),
+        ('TRUCK','Truck'),
+    ]
+    type = models.CharField(max_length=10,choices=TYPES,default="SEDAN")
+    year = models.IntegerField(default=2023, validators=[MaxValueValidator(2023),MinValueValidator(2015)])
+    def __str__(self):
+        return self.name
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
