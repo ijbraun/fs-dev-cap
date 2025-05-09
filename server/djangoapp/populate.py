f9ff10c696690a1536c789699c7ede02ab6de918
ijbraun <9153212+ijbraun@users.noreply.github.com>
2025-05-09 14:12:34 -0400

    linting

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -79,8 +79,11 @@ def initiate():
        "type": "SUV",
        "year": 2023,
        "make": car_make_instances[4]},
-        # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
-        CarModel.objects.create(name=data['name'], make=data['make'], type=data['type'], year=data['year'])
+        CarModel.objects.create(
+            name=data['name'],
+            make=data['make'],
+            type=data['type'],
+            year=data['year'])
